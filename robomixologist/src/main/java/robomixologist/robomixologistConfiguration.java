f26f6adb7442ff812e09372e20058f91fcc79f02
Kristin Smith <kristsmith@gmail.com>
2020-01-24 07:32:34 -0700

    Initial project setup

---
+++ robomixologist/src/main/java/robomixologist/robomixologistConfiguration.java
@@ -0,0 +1,10 @@
+package robomixologist;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class robomixologistConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
