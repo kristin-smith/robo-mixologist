c9ee6e457d1b6a23d0620246c60044557d37003a
Kristin Smith <kristsmith@gmail.com>
2020-01-25 13:51:48 -0700

    added hello-world.yml and added get/setters to template and defaultName in robomixologistConfiguration.java

--- robomixologist/src/main/java/robomixologist/robomixologistConfiguration.java
+++ robomixologist/src/main/java/robomixologist/robomixologistConfiguration.java
@@ -2,9 +2,32 @@ package robomixologist;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotEmpty;
 
 public class robomixologistConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate(){
+        this.template = template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template){
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName(){
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name){
+        this.defaultName = name;
+    }
 }
